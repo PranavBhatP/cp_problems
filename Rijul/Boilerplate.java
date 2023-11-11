3680e8b0ec33d802398c46a8e5f9bf350a6439b2
CyberKraft23 <rijulraman23@gmail.com>
2023-11-11 22:23:02 +0530

    Added boilerplate code for JAVA

---
+++ Rijul/Boilerplate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Boilerplate{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            //Write your code here
+        }
+        sc.close();
+    }
+}
