6c60fa9a74421519e117448939f6e28276751efb
Calisto-Mathias <calisto.mathias.25@gmail.com>
2023-11-15 16:54:24 +0530

    Small Update

--- Calisto/Solutions/AppendOr/AppendOr.cpp
+++ Calisto/Solutions/AppendOr/AppendOr.cpp
@@ -1,72 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define st size_t
-#define rep(a,b) for(int i = a;i<b;i++)
-#define repi(a,b) for(int i = a;i<b;i++)
-#define repj(a,b) for(int j = a;j<b;j++)
-#define vi vector<int>
+// Function to solve the problem for a single test case
+int solve() {
+    // Input the number of elements (n) and the target value (y)
+    int n, y;
+    cin >> n >> y;
 
-#define output(a) {for(int i = 0;i<n;i++) cout<<a[i]<<" "; cout<<endl;}
-#define input(a) for(int i = 0;i<n;i++) {cin>>a[i];}
+    // Input the array of integers
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 
-/*
-                                     d88b
-                     _______________|8888|_______________
-                    |_____________ ,~~~~~~. _____________|
-  _________         |_____________: mmmmmm :_____________|         _________
- / _______ \   ,----|~~~~~~~~~~~,'\ _...._ /`.~~~~~~~~~~~|----,   / _______ \
-| /       \ |  |    |       |____|,d~    ~b.|____|       |    |  | /       \ |
-||         |-------------------\-d.-~~~~~~-.b-/-------------------|         ||
-||         | |8888 ....... _,===~/......... \~===._         8888| |         ||
-||         |=========_,===~~======._.=~~=._.======~~===._=========|         ||
-||         | |888===~~ ...... //,, .`~~~~'. .,\\        ~~===888| |         ||
-||        |===================,P'.::::::::.. `?,===================|        ||
-||        |_________________,P'_::----------.._`?,_________________|        ||
-`|        |-------------------~~~~~~~~~~~~~~~~~~-------------------|        |'
-  \_______/                                           _ Cal2509 _  \_______/
-  
-*/
+    // Initialize 'out' with the first element of the array
+    int out = a[0];
 
-int solve(){
-	st n,y;
-	cin>>n>>y;
-	st a[n];
-	input(a);
-	int out = a[0];
-	rep(0,n){
-		out |= a[i];
-	}
-	if(out>y){
-		cout<<"-1"<<endl;
-		return 0;
-	}
-	else{
-		st diff = y - out;
-		st diff_copy = diff;
-		while(diff>=1){
-			st power = log2(diff);
-			st mask = 1<<power;
-			if((out|mask)==out){
-				cout<<"-1"<<endl;
-				return 0;
-			}
-			out|=mask;
-			diff = y - out;
-		}
-		cout<<diff_copy<<endl;
-		return 0;
-	}
-	
+    // Iterate through the array to find bitwise OR of all elements
+    for (int i = 0; i < n; i++) {
+        out = out | a[i];
+    }
+
+    // Check if the bitwise OR result is greater than the target value
+    if (out > y) {
+        cout << "-1" << endl;
+        return 0;
+    } else {
+        // Calculate the difference between the target value and the bitwise OR result
+        int diff = y - out;
+        int diff_copy = diff;
+
+        // Iterate while the difference is greater than or equal to 1
+        while (diff >= 1) {
+            // Find the highest bit set in the difference
+            int power = log2(diff);
+            
+            // Create a mask with only the bit at 'power' set to 1
+            int mask = 1 << power;
+
+            // Check if setting this bit in 'out' maintains its value
+            if ((out | mask) == out) {
+                cout << "-1" << endl;
+                return 0;
+            } else {
+                // Set the bit at 'power' in 'out'
+                out = out | mask;
+
+                // Update the difference
+                diff = y - out;
+            }
+        }
+
+        // Output the original difference
+        cout << diff_copy << endl;
+        return 0;
+    }
 }
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	st t;
-	cin>>t;
-	while(t--)
-		solve();
-	return 0;
+// Main function
+int main() {
+    // Input the number of test cases
+    int t;
+    cin >> t;
+
+    // Iterate through each test case and call the solve function
+    while (t--) {
+        solve();
+    }
+
+    return 0;
 }
