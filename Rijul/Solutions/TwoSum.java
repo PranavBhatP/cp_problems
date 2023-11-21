f58c8005b41f8a7bf164b3c47245bc9ed3ec5b48
CyberKraft23 <rijulraman23@gmail.com>
2023-11-21 22:50:34 +0530

    added solutions

---
+++ Rijul/Solutions/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
+        int[] copy = Arrays.copyOf(nums,nums.length);
+        int[] res = new int[2];
+        Arrays.sort(copy);
+        int lower = 0, upper = nums.length - 1;
+        while(lower<upper){
+            if((copy[lower]+copy[upper])>target){
+                upper--;
+            }
+            else if((copy[lower]+copy[upper])<target){
+                lower++;
+            }
+            else{
+                for(int i=0;i<nums.length;i++){
+                    if(nums[i]==copy[lower]) res[0]=i;
+                }
+                for(int j=0;j<nums.length;j++){
+                    if((j!=res[0]) && (nums[j]==copy[upper])) res[1]=j;
+                }
+                return res; 
+            } 
+        }
+        return new int[]{-1, -1};
+    }
+}
